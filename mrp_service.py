83f9db596698c55d3580fe8264488c9a301b88b9
valeria1404 <vale_nena1404@hotmail.com>
2015-09-28 21:17:10 -0500

    modlulo ok  con funciones de foto

--- mrp_service.py
+++ mrp_service.py
@@ -28,17 +28,103 @@ from tools.translate import _
 
 
 class mrp_service_all(osv.osv):
-	_name = 'mrp_service.all'
-	_description = 'Todas las ordenes de servicio'
-	_columns = {
-		'id'        : fields.integer('ID',required=True),
-		'name'      : fields.char('nombre de quien visita', size = 25, help = 'Nombre del servicio', required = True),		
-        'visit'     : fields.many2one('hr.employee', 'Nombre a quien visita', size = 25, required = True),
-        'reason'    : fields.char('Razon de la visita', size = 128, required = True),
-        'date'      : fields.datetime("Fecha"),
-        'photo'     : fields.binary('Photo'),
-        'photo_card': fields.binary('Photo card'),
-        'photo_car' : fields.binary('Photo car'),    
+    _name = 'mrp_service.all'
+    _description = 'Todas las ordenes de servicio'
 
+    def _get_image(self, cr, uid, ids, name, args, context=None):
+        result = dict.fromkeys(ids, False)
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = tools.image_get_resized_images(obj.image)
+        return result
+    
+    def _set_image(self, cr, uid, id, name, value, args, context=None):
+        return self.write(cr, uid, [id], {'image': tools.image_resize_image_big(value)}, context=context)
+ 
+    _columns = {
+		'id'          : fields.integer('ID',required=True),
+		'name'        : fields.char('Visitante', size = 25, help = 'Nombre del servicio', required = True),		
+        'visit'       : fields.many2one('hr.employee', 'Nombre a quien visita', size = 25, required = True),
+        'reason'      : fields.char('Razon de la visita', size = 128, required = True),
+        'date_out'    : fields.datetime("Hora de salida"),
+        'date'        : fields.datetime("Hora de entrada"),
+        'equipo'      : fields.char("Equipo o herramienta", size =25),
+        #'photo'     : fields.binary('Photo'),
+        'image'       : fields.binary("Photo",
+            help="This field holds the image used as photo for the visits, limited to 1024x1024px."),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+            string="Medium-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Medium-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image, with aspect ratio preserved. "\
+                 "Use this field in form views or some kanban views."),
+        'image_small'  : fields.function(_get_image, fnct_inv=_set_image,
+            string="Small-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Small-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image, with aspect ratio preserved. "\
+                 "Use this field anywhere a small image is required."),
+       # 'photo_card': fields.binary('Photo card'),
+       'image_card'       : fields.binary("Photo",
+            help="This field holds the image_card used as photo for the visits, limited to 1024x1024px."),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+            string="Medium-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image_card'], 10),
+            },
+            help="Medium-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image_card, with aspect ratio preserved. "\
+                 "Use this field in form views or some kanban views."),
+        'image_small'  : fields.function(_get_image, fnct_inv=_set_image,
+            string="Small-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image_card'], 10),
+            },
+            help="Small-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image_card, with aspect ratio preserved. "\
+                 "Use this field anywhere a small image_card is required."),
+       # 'photo_car' : fields.binary('Photo car'),
+        'image'       : fields.binary("Photo",
+            help="This field holds the image used as photo for the visits, limited to 1024x1024px."),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+            string="Medium-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Medium-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image, with aspect ratio preserved. "\
+                 "Use this field in form views or some kanban views."),
+        'image_small'  : fields.function(_get_image, fnct_inv=_set_image,
+            string="Small-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+            },
+            help="Small-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image, with aspect ratio preserved. "\
+                 "Use this field anywhere a small image is required."),
+       # 'photo_card': fields.binary('Photo card'),
+       'image_car'       : fields.binary("Photo",
+            help="This field holds the image_car used as photo for the visits, limited to 1024x1024px."),
+        'image_medium': fields.function(_get_image, fnct_inv=_set_image,
+            string="Medium-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image_card'], 10),
+            },
+            help="Medium-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image_card, with aspect ratio preserved. "\
+                 "Use this field in form views or some kanban views."),
+        'image_small'  : fields.function(_get_image, fnct_inv=_set_image,
+            string="Small-sized photo", type="binary", multi="_get_image",
+            store = {
+                'mrp_service': (lambda self, cr, uid, ids, c={}: ids, ['image_car'], 10),
+            },
+            help="Small-sized photo of the employee. It is automatically "\
+                 "resized as a 64x64px image_car, with aspect ratio preserved. "\
+                 "Use this field anywhere a small image_car is required.")  
+
+        
     }
 mrp_service_all()
